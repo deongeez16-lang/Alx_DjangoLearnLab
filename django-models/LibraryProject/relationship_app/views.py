9173be9f832fc9cc489471174cf78762da2a7026
deongeez16-lang <deongeez16@gmail.com>
2026-01-25 11:45:46 -0800

    Fix import statement for ALX checker (from .models import Library)

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
-
-# Function-based view: list all books
-def list_books(request):
+import renderst_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
@@ -15,6 +13,9 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+ews.py
+cd /Alx_DjangoLearnLab/django-models/LibraryProject/relationship_app
+vi views.py
 
 
 
