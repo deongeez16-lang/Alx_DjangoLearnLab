fca13969dd77a8b689a413657f3329bf7641c294
deongeez16-lang <deongeez16@example.com>
2026-02-03 14:07:16 +0300

    Auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,28 @@
 from django.urls import path
 # from .views import list_books, LibraryDetailView
 from . import views
-from .views import UserLoginView, UserLogoutView, register
-
+# from .views import UserLoginView, UserLogoutView, register
+from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),  # function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
+    # path('login/', UserLoginView.as_view(), name='login'),
+    # path('logout/', UserLogoutView.as_view(), name='logout'),
+    # path('register/', register, name='register'),
+       path(
+        'login/',
+        LoginView.as_view(template_name='relationship_app/login.html'),
+        name='login'
+    ),
+    path(
+        'logout/',
+        LogoutView.as_view(template_name='relationship_app/logout.html'),
+        name='logout'
+    ),
+    path(
+        'register/',
+        views.register,
+        name='register'
+    ),
 ]
 
